013df012fcc37464d60c2e283f706f7e83d1b4c9
afmireski <afmireski@outlook.com>
2023-01-28 09:11:35 -0300

    #3 - Implement data input

--- rust-converter/src/main.rs
+++ rust-converter/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("###############################################");
+    println!("# Binary Converter - RUST\n\n");
+
+    let mut input = String::new();
+
+    println!("Type any float number:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let input: f32 = input.trim().parse().expect("Please try again and type a float number");
 }
