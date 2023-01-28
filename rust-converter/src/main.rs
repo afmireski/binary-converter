9d61ebb6930bebb6b834da414e254a6e7524a042
afmireski <afmireski@outlook.com>
2023-01-28 09:53:33 -0300

    #3 - Separacao da parte decimal e inteira da entrada

--- rust-converter/src/main.rs
+++ rust-converter/src/main.rs
@@ -1,4 +1,31 @@
 use std::io;
+use std::str;
+
+fn get_int_value(n: f32) -> i32 {
+    let s = n.to_string();
+
+    let mut slice = &s[..];
+    for (i, &c) in s.as_bytes().iter().enumerate() {
+        if c == b'.' {
+            slice = &s[0..i];
+            break;
+        }
+    }
+
+    slice.parse::<i32>().expect("Fail on get int value")
+}
+
+fn convertFloatToBinary(mut number: f32) {
+    let mut has_signal: u8 = 0;
+
+    if number < 0.0 {
+        has_signal = 1;
+        number = -number;
+    }
+
+    let int_value = get_int_value(number);
+    let float_value = number - (int_value as f32);
+}
 
 fn main() {
     println!("###############################################");
@@ -11,5 +38,8 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    let input: f32 = input.trim().parse().expect("Please try again and type a float number");
+    let input: f32 = input
+        .trim()
+        .parse()
+        .expect("Please try again and type a float number");
 }
