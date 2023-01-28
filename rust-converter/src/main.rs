1679f3127ef2c7429a598ad29c7eba0475846406
afmireski <afmireski@outlook.com>
2023-01-28 14:02:35 -0300

    #3 - Implement function convert_int_to_binary

--- rust-converter/src/main.rs
+++ rust-converter/src/main.rs
@@ -1,5 +1,4 @@
-use std::io;
-use std::str;
+use std::{io, num};
 
 fn get_int_value(n: f32) -> i32 {
     let s = n.to_string();
@@ -15,7 +14,50 @@ fn get_int_value(n: f32) -> i32 {
     slice.parse::<i32>().expect("Fail on get int value")
 }
 
-fn convertFloatToBinary(mut number: f32) {
+fn get_bit_len(number: i32) -> u8 {
+    let mut i: u8 = 0;
+
+    while number >= 2_i32.pow(i.into()) {
+        i += 1
+    }
+
+    return i;
+}
+
+fn convert_int_to_binary(number: i32) -> String {
+    let bits = get_bit_len(number);
+
+    let mut result = String::from("");
+
+    if number == 0 {
+        return String::from("0");
+    }
+
+    let mut max_value = 2_i32.pow(bits.into());
+    let mut value = number;
+
+    while value > 0 {
+        if value >= max_value {
+            result.push('1');
+            value -= max_value;
+        } else {
+            result.push('0');
+        }
+        max_value /= 2;
+    }
+
+    if &result[0..1] == "0" {
+        result = (&result[1..]).to_string();
+    }
+
+    while result.len() < bits.into() {
+        result.push('0');
+    }
+
+    result
+}
+
+fn convert_float_to_binary(mut number: f32) {
     let mut has_signal: u8 = 0;
 
     if number < 0.0 {
@@ -25,6 +67,8 @@ fn convertFloatToBinary(mut number: f32) {
 
     let int_value = get_int_value(number);
     let float_value = number - (int_value as f32);
+
+    let binary_int_value = convert_int_to_binary(int_value);
 }
 
 fn main() {
@@ -42,4 +86,6 @@ fn main() {
         .trim()
         .parse()
         .expect("Please try again and type a float number");
+
+    convert_float_to_binary(input);
 }
